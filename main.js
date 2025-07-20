573836f67c5419831151a369f7b8a9b36477247f
GigaCala <marilyntiaya@gmail.com>
2025-07-20 20:27:59 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+var typed = new Typed('.text', {
+    strings: [
+        'Essential items',
+        'Affordable prices',
+        'High quality'
+    ],
+    typeSpeed: 100,       // Speed of typing in milliseconds
+    backSpeed: 100,       // Speed of backspacing
+    backDelay: 1000,      // Delay before backspacing starts
+    loop: true            // Loop the animation infinitely
+});
+
+
+
